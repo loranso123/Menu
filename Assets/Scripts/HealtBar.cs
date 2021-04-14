70ada8fcc88bb1a702e2c372a2ded8a00902f1a5
Константин Кучинский <krims12345@gmail.com>
2021-04-15 00:03:13 +0300

    Initial commit

---
+++ Assets/Scripts/HealtBar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealtBar : MonoBehaviour
+{
+    [SerializeField] private float fillSpeed = 50.0f;
+    [SerializeField] private Text _textSlider;
+
+    private Slider _slider;
+    private RectTransform _fillRect;
+    private float _targetValue = 0f;
+    private float _curValue = 0f;
+
+    void Awake()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.onValueChanged.AddListener(delegate { ValueChange(); });
+        _fillRect = _slider.fillRect;
+        _targetValue = _curValue = _slider.value;
+    }
+
+    public void ValueChange()
+    {
+        _targetValue = _slider.value;
+        _textSlider.text = _slider.value.ToString();
+    }
+
+    void Update()
+    {
+        _curValue = Mathf.MoveTowards(_curValue, _targetValue, Time.deltaTime * fillSpeed);
+
+        Vector2 fillAnchor = _fillRect.anchorMax;
+        fillAnchor.x = Mathf.Clamp01(_curValue / _slider.maxValue);
+        _fillRect.anchorMax = fillAnchor;
+    }
+
+    private void OnDestroy()
+    {
+        _slider.onValueChanged.RemoveAllListeners();
+    }
+
+    public void Attack()
+    {
+        _slider.value -= 10f;
+    }
+
+    public void Heal()
+    {
+        _slider.value += 10f;
+    }
+}
