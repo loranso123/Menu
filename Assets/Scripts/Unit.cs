974ac3406ef82dcfa64d56d14042286393ca59f5
Константин Кучинский <krims12345@gmail.com>
2021-04-17 01:43:28 +0300

    fix
    fix

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+public class Unit : MonoBehaviour
+{
+    public event Action<int> OnHealthChanged;
+
+    [SerializeField] private int _health = 100;
+
+    public int Health => _health;
+
+    public void TakeAttack(int damage)
+    {
+        if (_health > 0)
+        {
+            _health -= damage; 
+            OnHealthChanged?.Invoke(_health);
+        }
+    }
+
+    public void TakeHeal(int heal)
+    {
+        if (_health < 100)
+        {
+            _health += heal; 
+            OnHealthChanged?.Invoke(_health);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitHealthBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class UnitHealthBar : MonoBehaviour
+{
+    [SerializeField] private Unit _target;
+    [SerializeField]private float fillSpeed = 50.0f;
+    private float _targetValue;
+    private Slider _bar;
+    private Coroutine _updateValueCoroutine;
+
+    private void Awake()
+    {
+        _bar = GetComponent<Slider>();
+    }
+
+    private void OnEnable()
+    {
+        _target.OnHealthChanged += UpdateValue;
+        UpdateValue(_target.Health);
+    }
+
+    private void OnDisable()
+    {
+        _target.OnHealthChanged -= UpdateValue;
+    }
+
+    private void UpdateValue(int value)
+    {
+        _targetValue = value;
+        if (_updateValueCoroutine == null)
+        {
+           _updateValueCoroutine = StartCoroutine(UpdateValueCoroutine());
+        }
+    }
+
+    private IEnumerator UpdateValueCoroutine()
+    {
+        while (_bar.value != _targetValue)
+        {
+            _bar.value = Mathf.MoveTowards(_bar.value, _targetValue, Time.deltaTime * fillSpeed);
+            yield return new WaitForSecondsRealtime(Time.deltaTime);
+        }
+        _updateValueCoroutine = null;
+    }
+}
